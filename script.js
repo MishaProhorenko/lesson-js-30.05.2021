4e0103faacc2428e208f093ebba538032c531a64
Misha Prohorenko <mishaprohorenko96@gmail.com>
2021-05-30 20:06:43 +0300

    change docum

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 'use strict';
 
 
+let countOfClicks = 0;
+count.innerText = countOfClicks;
+btnPlus.addEventListener('click', () => {
+	count.innerText = ++countOfClicks;
+})
+btnMinus.addEventListener('click', () => {
+	count.innerText = --countOfClicks;
+})
+// btnRed.addEventListener('click', () => {
+// 	divCount.className = 'red'
+// })
+// btnGreen.addEventListener('click', () => {
+// 	divCount.className = 'green'
+// })
+// btnBlue.addEventListener('click', () => {
+// 	divCount.className = 'blue'
+// })
+
+let getCollectionBtn = ()=>{
+	let btns = document.body.querySelectorAll('button');
+	console.log(btns)
+	for (let i = 0; i < btns.length; i++) {
+		if (btns[i].className === 'red') {
+			btns[i].addEventListener('click', () => {
+				divCount.style.color = 'red'
+			})
+		}
+		if (btns[i].className === 'green') {
+			btns[i].addEventListener('click', () => {
+				divCount.style.color = 'green'
+			})
+		}
+		if (btns[i].className === 'blue') {
+			btns[i].addEventListener('click', () => {
+				divCount.style.color = 'blue'
+			})
+		}
+	}
+}
+getCollectionBtn()
+
+
+
+
 
